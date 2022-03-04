afff05afdb71ed63e367576e18c29822b06fe691
Drayer Sivertsen <82745871+DrayerSivertsen@users.noreply.github.com>
2022-03-04 12:31:58 -0800

    Add files via upload

---
+++ Nasa API/nasaapi.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import urllib.request
+import json
+import webbrowser
+import urllib
+from flask import Flask, render_template
+from PIL import Image
+import base64
+import io
+
+
+
+app = Flask(__name__)
+
+# Define APOD
+apodurl = 'https://api.nasa.gov/planetary/apod?'
+mykey = 'api_key=ZuohWEJ9pX59O6nuwvw0EvkYgN2EOdkkSZagtn7x'
+
+# Call the webservice
+apodurlobj = urllib.request.urlopen(apodurl + mykey)
+
+# read the file-like object
+apodread = apodurlobj.read()
+
+# decode json to python data structure
+decodeapod = json.loads(apodread.decode('utf-8'))
+
+# display our pythonic data
+print("\n\nConverted python data")
+print(decodeapod)
+
+# use web browser to open the HTTPS URL
+#input('\nPress Enter to open NASA Picture of the Day in Google')
+#webbrowser.open(decodeapod['url'])
+
+
+image_url = str(decodeapod['url'])
+name = decodeapod['explanation']
+
+@app.route('/')
+def image():
+    return render_template("index.html", image_url=image_url)
+
+@app.route('/description')
+def des():
+    return render_template("index.html", des=name)
+
+
+
+if __name__ == '__main__':
+    app.run()
